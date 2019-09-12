0800e2fee2718a8292f76ea2c4722635e28aef54
dell <dell@DESKTOP-236LIIB>
2019-09-12 23:51:54 +0100

    lot of change hhh: home/login2/css/scss

---
+++ public_html/home.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Icha3ah-home</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700" rel="stylesheet">
+
+        <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/magnific-popup.css">
+        <link rel="stylesheet" href="css/jquery-ui.css">
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+        <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+        <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+        <link rel="stylesheet" href="css/aos.css">
+
+        <link rel="stylesheet" href="css/style.css">        
+        <link rel="stylesheet" href="ussef.css">
+
+
+    </head>
+    <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+        <div class="site-wrap"  id="home-section">
+
+            <div class="site-mobile-menu site-navbar-target">
+                <div class="site-mobile-menu-header">
+                    <div class="site-mobile-menu-close mt-3">
+                        <span class="icon-close2 js-menu-toggle"></span>
+                    </div>
+                </div>
+                <div class="site-mobile-menu-body"></div>
+            </div>
+
+
+            <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+
+                <div class="container">
+                    <div class="row align-items-center position-relative">
+
+
+                        <div class="site-logo">
+                            <a href="index.html" class="text-black"><span class="half1">ICHA3</span>AH</a>
+                        </div>
+
+                        <div class="col-12">
+                            <nav class="site-navigation text-center " role="navigation">
+
+                                <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                                    <li><a href="home.php" class="nav-link active">Home</a></li>
+                                    <li><a href="#services-section" class="nav-link">Public rumors</a></li>
+                                    <li><a href="re-rumors.php" class="nav-link">Received rumors</a></li>
+                                    <li><a href="#team-section" class="nav-link">My profile</a></li>
+                                    <li><a href="login2.php" class="nav-link">Login</a></li>
+                                    <li><a href="#contact-section" class="nav-link">Contact</a></li>
+                                </ul>
+                            </nav>
+
+                        </div>
+
+                        <div class="toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+                    </div>
+                </div>
+
+            </header>
+
+
+            <div class="site-section-cover img-bg-section" style="background-image: url('ussefpic/rumor1.jpg'); " data-aos="fade">
+
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-5">
+
+                            <div class="box-shadow-content">
+                                <div class="block-heading-1">
+                                    <!--<span class="d-block mb-3"  data-aos="fade-up">Welcome To Toothsy</span>-->
+                                    <h1 class="mb-4 mainTextColor1" data-aos="fade-up" data-aos-delay="100">Share rumors with your friends and the whole world</h1>
+                                </div>
+
+                                <p class="mb-4" data-aos="fade-up" data-aos-delay="200" style="color: black !important" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet quod modi, eius minus hic illum perspiciatis! Voluptate facere tempore, libero enim fugit, temporibus non dolor nostrum iure ad. Dolorem, explicabo!</p>
+                                <p data-aos="fade-up" data-aos-delay="300"><a href="#" class="btn mainBackColor2 text-white py-3 px-5">Create account now</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- <div class="img-absolute">
+                  <img src="images/person-transparent-2.png" alt="Image" class="img-fluid">
+                </div> -->
+            </div>
+
+
+            <div class="site-section" id="blog-section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 text-center mb-5">
+                            <div class="block-heading-1">
+                                <!--<span>Latest Blog Posts</span>-->
+                                <h2 class="mainTextColor2">Rumors of these days</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <div>
+                                <a href="single.html" class="mb-4 d-block"><img src="ussefpic/examp1.jpg" alt="Image" class="img-fluid rounded"></a>
+                                <h2><a href="single.html" class="mainTextColor2">Hatake kakashi crying</a></h2>
+                                <p class="text-muted mb-3 text-uppercase small"><span class="mr-2">September 12, 2019</span> By <a href="#"  class="by">admin</a></p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat et suscipit iste libero neque. Vitae quidem ducimus voluptatibus nemo cum odio ab enim nisi, itaque, libero fuga veritatis culpa quis!</p>
+                                <p><a href="single.html">Read More</a></p>
+                            </div>
+                        </div>
+                        <div class="col-lg-1">
+
+                        </div>
+                        <div class="col-lg-5">
+                            <div>
+                                <a href="single.html" class="mb-4 d-block"><img src="ussefpic/examp2.jpg" alt="Image" class="img-fluid rounded"></a>
+                                <h2><a href="single.html" class="mainTextColor2">Tony Strak is alive</a></h2>
+                                <p class="text-muted mb-3 text-uppercase small"><span class="mr-2">September 12, 2019</span> By <a href="#" class="by">admin</a></p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat et suscipit iste libero neque. Vitae quidem ducimus voluptatibus nemo cum odio ab enim nisi, itaque, libero fuga veritatis culpa quis!</p>
+                                <p><a href="single.html">Read More</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 text-center mt-5">
+                            <div class="block-heading-1">
+                                <span> see all rumors</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="site-section bg-light" id="contact-section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 text-center mb-5">
+                            <div class="block-heading-1">
+                                <span>have a problem ?</span>
+                                <h2 class="mainTextColor2">Contact Us</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 mb-5">
+                            <form action="#" method="post">
+                                <div class="form-group row">
+                                    <div class="col-md-6 mb-4 mb-lg-0">
+                                        <input type="text" class="form-control" placeholder="First name">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <input type="text" class="form-control" placeholder="First name">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <div class="col-md-12">
+                                        <input type="text" class="form-control" placeholder="Email address">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <div class="col-md-12">
+                                        <textarea name="" id="" class="form-control" placeholder="Write your message." cols="30" rows="10"></textarea>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-md-6 ml-auto">
+                                        <input type="submit" class="btn btn-block mainBackColor2 text-white py-3 px-5" value="Send Message">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-lg-4 ml-auto">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <footer class="site-footer">
+                <div class="container">
+                    <div class="row  text-center">
+                        <div class="col-md-12">
+                            <div class="">
+                                <p>
+                                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved for <a href="#">ICHA3AH</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+        </div>
+
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src="js/jquery-ui.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/jquery.countdown.min.js"></script>
+        <script src="js/jquery.magnific-popup.min.js"></script>
+        <script src="js/bootstrap-datepicker.min.js"></script>
+        <script src="js/jquery.sticky.js"></script>
+        <script src="js/jquery.waypoints.min.js"></script>
+        <script src="js/jquery.animateNumber.min.js"></script>
+        <script src="js/aos.js"></script>
+
+        <script src="js/main.js"></script>
+
+    </body>
+</html>
---
+++ public_html/re-rumors.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>rumors on me</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700" rel="stylesheet">
+
+        <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/magnific-popup.css">
+        <link rel="stylesheet" href="css/jquery-ui.css">
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+        <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+        <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+        <link rel="stylesheet" href="css/aos.css">
+
+        <link rel="stylesheet" href="css/style.css">
+
+        <link rel="stylesheet" href="ussef.css">
+
+
+    </head>
+    <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+        <div class="site-wrap"  id="home-section">
+
+            <div class="site-mobile-menu site-navbar-target">
+                <div class="site-mobile-menu-header">
+                    <div class="site-mobile-menu-close mt-3">
+                        <span class="icon-close2 js-menu-toggle"></span>
+                    </div>
+                </div>
+                <div class="site-mobile-menu-body"></div>
+            </div>
+
+
+            <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+
+                <div class="container">
+                    <div class="row align-items-center position-relative">
+
+
+                        <div class="site-logo">
+                            <a href="index.html" class="text-black"><span class="half1">ICHA3</span>AH</a>
+                        </div>
+
+                        <div class="col-12">
+                            <nav class="site-navigation text-center " role="navigation">
+
+                                <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                                    <li><a href="home.php" class="nav-link">Home</a></li>
+                                    <li><a href="#services-section" class="nav-link">Public rumors</a></li>
+                                    <li><a href="re-rumors.php" class="nav-link active">Received rumors</a></li>
+                                    <li><a href="#team-section" class="nav-link">My profile</a></li>
+                                    <li><a href="login2.php" class="nav-link">Login</a></li>
+                                    <li><a href="#contact-section" class="nav-link">Contact</a></li>
+                                </ul>
+                            </nav>
+
+                        </div>
+
+                        <div class="toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+                    </div>
+                </div>
+
+            </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <div class="site-section bg-light block-13" id="testimonials-section" data-aos="fade">
+                <div class="container">
+
+                    <div class="text-center mb-5">
+                        <div class="block-heading-1">
+                            <span>Ready to be shocked ?</span>
+                            <h2 class="mainTextColor2">rumors on me</h2>
+                        </div>
+                    </div>
+
+                    <!--                    <div class="owl-carousel nonloop-block-13">
+                                            <div>
+                                                <div class="block-testimony-1 text-center">
+                    
+                                                    <blockquote class="mb-4">
+                                                        <p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, fugit excepturi sapiente voluptatum nulla odio quaerat quibusdam tempore similique doloremque veritatis et cupiditate, maiores cumque repudiandae explicabo tempora deserunt consequuntur?&rdquo;</p>
+                                                    </blockquote>
+                    
+                                                    <figure>
+                                                        <img src="ussefpic/unknown.png" alt="Image" class="img-fluid rounded-circle mx-auto">
+                                                    </figure>
+                                                    <h3 class="font-size-20 text-black">unknown friend</h3>
+                                                </div>
+                                            </div>
+                                        </div>-->
+                    <div class="row">
+                        <div class="col-lg-3">
+
+                        </div>
+                        <div class="col-lg-6">
+                            <div>
+                                <div class="block-testimony-1 text-center">
+
+                                    <blockquote class="mb-4">
+                                        <p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, fugit excepturi sapiente voluptatum nulla odio quaerat quibusdam tempore similique doloremque veritatis et cupiditate, maiores cumque repudiandae explicabo tempora deserunt consequuntur?&rdquo;</p>
+                                    </blockquote>
+
+                                    <figure>
+                                        <img src="ussefpic/unknown.png" alt="Image" class="img-fluid rounded-circle mx-auto">
+                                    </figure>
+                                    <h3 class="font-size-20 text-black">unknown friend 1</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-3">
+
+                        </div>
+                        <div class="col-lg-6">
+                            <div>
+                                <div class="block-testimony-1 text-center">
+
+                                    <blockquote class="mb-4">
+                                        <p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, fugit excepturi sapiente voluptatum nulla odio quaerat quibusdam tempore similique doloremque veritatis et cupiditate, maiores cumque repudiandae explicabo tempora deserunt consequuntur?&rdquo;</p>
+                                    </blockquote>
+
+                                    <figure>
+                                        <img src="ussefpic/unknown.png" alt="Image" class="img-fluid rounded-circle mx-auto">
+                                    </figure>
+                                    <h3 class="font-size-20 text-black">unknown friend 2</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-3">
+
+                        </div>
+                        <div class="col-lg-6">
+                            <div>
+                                <div class="block-testimony-1 text-center">
+
+                                    <blockquote class="mb-4">
+                                        <p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, fugit excepturi sapiente voluptatum nulla odio quaerat quibusdam tempore similique doloremque veritatis et cupiditate, maiores cumque repudiandae explicabo tempora deserunt consequuntur?&rdquo;</p>
+                                    </blockquote>
+
+                                    <figure>
+                                        <img src="ussefpic/unknown.png" alt="Image" class="img-fluid rounded-circle mx-auto">
+                                    </figure>
+                                    <h3 class="font-size-20 text-black">unknown friend 3</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+            <footer class="site-footer">
+                <div class="container">
+                    <div class="row  text-center">
+                        <div class="col-md-12">
+                            <div class="">
+                                <p>
+                                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved for <a href="#">ICHA3AH</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+
+        </div>
+
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src="js/jquery-ui.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/jquery.countdown.min.js"></script>
+        <script src="js/jquery.magnific-popup.min.js"></script>
+        <script src="js/bootstrap-datepicker.min.js"></script>
+        <script src="js/jquery.sticky.js"></script>
+        <script src="js/jquery.waypoints.min.js"></script>
+        <script src="js/jquery.animateNumber.min.js"></script>
+        <script src="js/aos.js"></script>
+
+        <script src="js/main.js"></script>
+
+    </body>
+</html>
